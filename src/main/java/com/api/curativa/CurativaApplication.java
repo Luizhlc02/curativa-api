14b04f4d65efcb4b1c57db21cf34629b9254d6c4
Luiz Henrique <luizhlc02@gmail.com>
2025-06-07 16:33:51 -0300

    first commit

---
+++ src/main/java/com/api/curativa/CurativaApplication.java
@@ -0,0 +1,13 @@
+package com.api.curativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/curativa/CurativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.curativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
